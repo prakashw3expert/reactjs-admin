cbae526de4b3ec7a190c13a8cfb08575ee6d5674
Prakash Saini <prakashw3expert@gmaill.com>
2017-05-23 09:13:52 +0530

    Client Manager, Trainer Manager

---
+++ src/Config/AppConfig.js
@@ -0,0 +1,7 @@
+// Simple React Native specific changes
+
+export default {
+  // font scaling override - RN default is on
+  //ApiUrl: 'http://localhost:3001/api/v1/'
+  ApiUrl: 'http://139.162.56.185:3000/api/v1/'
+}
